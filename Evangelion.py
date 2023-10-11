b75653b22ec5206d92f78964d7d7f8ce20e2a13d
Dantjo <145995803+Dantjo@users.noreply.github.com>
2023-10-11 16:32:42 +0300

    Update Evangelion.py

--- Evangelion.py
+++ Evangelion.py
@@ -1,131 +1,193 @@
+#https://github.com/Dantjo/Evangelion.git
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QButtonGroup, QHBoxLayout, QVBoxLayout, QLabel, QRadioButton, QMessageBox, QGroupBox
+from PyQt5.QtWidgets import (
+        QApplication, QWidget, 
+        QHBoxLayout, QVBoxLayout, 
+        QGroupBox, QButtonGroup, QRadioButton,  
+        QPushButton, QLabel)
+
 
 class Question():
-    def __init__(self, question, war_1, war_2, war_3):
+    def __init__(self, question, right_answer, wrong1, wrong2, wrong3):
+        # все строки надо задать при создании объекта, они запоминаются в свойства
         self.question = question
-        self.war_1 = war_1
-        self.war_2 = war_2
-        self.war_3 = war_3
-        
-question_list = []
-question_list_2 = []
-question_list.append(Question('Объявлена тревога! Выберите пилота!','Аянами Рей','С.Аска Ленгли','Синдзи Икари'))
-question_list.append(Question('Выберите Евангелион','Eva-00','Eva-01','Eva-02'))
-question_list.append(Question('Проведение диагностики','','',''))
-question_list.append(Question('Подача LCL','','',''))
-question_list.append(Question('Подключение контактного комбинезона','','',''))
-question_list.append(Question('Синхронизация пилота','','',''))
-question_list.append(Question('Диагностика завершена','','',''))
-question_list.append(Question('!Запуск Евангелиона!','','',''))
-question_list_2.append(Question('Вы обнаружили ангела, командующий Икари дал приказ - уничтожить ангела, выберите действие:',
-    'Использоать позитронную винтовку','Использовать виброкинжал','Вступить в рукопашный бой'))
+        self.right_answer = right_answer
+        self.wrong1 = wrong1
+        self.wrong2 = wrong2
+        self.wrong3 = wrong3
+
+
+questions_list = [] 
+questions_list.append(Question('Объявлена тревога! Выберите пилота!','Аянами Рей','С.Аска Ленгли','Синдзи Икари',''))
+questions_list.append(Question('Выберите Евангелион','Eva-00','Eva-01','Eva-02',''))
+questions_list.append(Question('Проведение диагностики','далее','далее','далее','далее'))
+questions_list.append(Question('Подача LCL','далее','далее','далее','далее'))
+questions_list.append(Question('Подключение контактного комбинезона','далее','далее','далее','далее'))
+questions_list.append(Question('Синхронизация пилота','далее','далее','далее','далее'))
+questions_list.append(Question('Диагностика завершена','далее','далее','далее','далее'))
+questions_list.append(Question('!Запуск Евангелиона!','далее','далее','далее','далее'))
+questions_list.append(Question('Вы обнаружили ангела, командующий Икари дал приказ - уничтожить ангела, выберите действие:','Использоать позитронную винтовку','Использовать виброкинжал','Вступить в рукопашный бой',''))
+questions_list.append(Question('Ангел был атакован и частично потерял боеспособность, продолжайте в том же духе:','Использовать позитронную винтовку','Использовать виброкинжал','Вступить в рукопашный бой',''))
+questions_list.append(Question('Ангел был атакован и полностью потерял боеспособность, нанесите решающий удар:','Использовать позитронную винтовку','Использовать виброкинжал','Вступить в рукопашный бой',''))
+
 app = QApplication([])
-window = QWidget()
-window_2 = QWidget()
-window.setWindowTitle('Project "Evangelion"')
-window_2.setWindowTitle('Project "Evangelion"')
-btn_OK = QPushButton('Answer')
-main_Question = QLabel('Какой национальности не существует?')
 
-R_G_B = QGroupBox('Варианты ответов')
-rbtn_1 = QRadioButton('Энцы')
-rbtn_2 = QRadioButton('Чулымцы')
-rbtn_3 = QRadioButton('Смурфы')
-layout_ans1 = QHBoxLayout()
-layout_ans2 = QVBoxLayout()
-layout_ans3 = QVBoxLayout()
-layout_ans2.addWidget(rbtn_1)
-layout_ans2.addWidget(rbtn_2)
-layout_ans3.addWidget(rbtn_3)
-layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3)
-R_G_B.setLayout(layout_ans1)
-layout_line1 = QHBoxLayout()
-layout_line2 = QHBoxLayout()
-layout_line3 = QHBoxLayout()
-layout_line1.addWidget(main_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
-layout_line2.addWidget(R_G_B)
-layout_line3.addStretch(1)
-layout_line3.addWidget(btn_OK, stretch = 2 )
-layout_line3.addStretch(1)
-layout_card = QVBoxLayout()
-layout_card.addLayout(layout_line1, stretch = 2)
-layout_card.addLayout(layout_line2, stretch = 8)
-layout_card.addStretch(1)
-layout_card.addLayout(layout_line3, stretch = 1)
-layout_card.addStretch(1)
-layout_card.setSpacing(5)
 
-layout_line1.addWidget(main_Question)
-layout_line2.addWidget(R_G_B) 
-R_G_B.hide()
-layout_line3.addWidget(btn_OK, stretch=2)
-def show_result():
-    ''' показать панель ответов '''
-    R_G_B.hide()
-    AnsGroupBox.show()
-    btn_OK.setText('Следующий вопрос')
-def show_question():
-    ''' показать панель вопросов '''
-    R_G_B.show()
-    btn_OK.setText('Ответить')
-    RadioGroup.setExclusive(False)
-    rbtn_1.setChecked(False)
-    rbtn_2.setChecked(False)
-    rbtn_3.setChecked(False)
-    
-    RadioGroup.setExclusive(True)
-def test():
-    if 'Ответить' == btn_OK.text():
-        show_result()
-    else:
-        show_question()
-answers = [rbtn_1, rbtn_2, rbtn_3]
-def ask(q: Question):
-    answers[0].setText(q.war_1)
-    answers[1].setText(q.war_2)
-    answers[2].setText(q.war_3)
-    main_Question.setText(q.question)
-    show_question()
+btn_OK = QPushButton('Выбрать')
+lb_Question = QLabel('Самый сложный вопрос в мире!')
 
-def next_question():
-    window.cur_question = window.cur_question + 1
-    if window.cur_question >= len(question_list):
-        window.cur_question = 0
-        window.close()
-    q = question_list[window.cur_question]
-    ask(q)
-def next_question_2():
-    window.cur_question_2= window.cur_question_2 + 1
-    if window.cur_question == len(question_list_2):
-        window.cur_question = 0
-        window_2.show()
-    q = question_list[window.cur_question_2]
-    ask(q)
-def click_OK():
-    if btn_OK.text() == 'Ответить':
-        next_question()
-def click_OK_2():
-    if btn_OK.text() == 'Ответить':
-        next_question_2()
-RadioGroup = QButtonGroup() 
+
+RadioGroupBox = QGroupBox("Варианты ответов")
+
+
+rbtn_1 = QRadioButton('Вариант 1')
+rbtn_2 = QRadioButton('Вариант 2')
+rbtn_3 = QRadioButton('Вариант 3')
+rbtn_4 = QRadioButton('Вариант 4')
+
+
+RadioGroup = QButtonGroup()
 RadioGroup.addButton(rbtn_1)
 RadioGroup.addButton(rbtn_2)
 RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+
+layout_ans1 = QHBoxLayout()   
+layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans3 = QVBoxLayout()
+layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3) # два ответа во второй столбец
+layout_ans3.addWidget(rbtn_4)
+
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+
+
+RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+
+
+AnsGroupBox = QGroupBox("Результат теста")
+lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
+lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
+AnsGroupBox.setLayout(layout_res)
+
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+
+
+layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+layout_line2.addWidget(RadioGroupBox)   
+layout_line2.addWidget(AnsGroupBox)  
+AnsGroupBox.hide() 
+
+
+layout_line3.addStretch(1)
+layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addStretch(1)
+
+
+layout_card = QVBoxLayout()
+
+
+layout_card.addLayout(layout_line1, stretch=2)
+layout_card.addLayout(layout_line2, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line3, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5) # пробелы между содержимым
+
+
+def show_result():
+    ''' показать панель ответов '''
+    RadioGroupBox.hide()
+    AnsGroupBox.show()
+    btn_OK.setText('Следующий вопрос')
+
+
+def show_question():
+    ''' показать панель вопросов '''
+    RadioGroupBox.show()
+    AnsGroupBox.hide()
+    btn_OK.setText('Выбрать')
+    # сбросить выбранную радио-кнопку
+    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    rbtn_1.setChecked(False)
+    rbtn_2.setChecked(False)
+    rbtn_3.setChecked(False)
+    rbtn_4.setChecked(False)
+    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
+
+
+answers = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+
+def ask(q: Question):
+    ''' функция записывает значения вопроса и ответов в соответствующие виджеты, 
+    при этом варианты ответов распределяются случайным образом'''
+    answers[0].setText(q.right_answer) # первый элемент списка заполним правильным ответом, остальные - неверными
+    answers[1].setText(q.wrong1)
+    answers[2].setText(q.wrong2)
+    answers[3].setText(q.wrong3)
+    lb_Question.setText(q.question) # вопрос
+    lb_Correct.setText(q.right_answer) # ответ 
+    show_question() # показываем панель вопросов 
+
+
+def show_correct(res):
+    ''' показать результат - установим переданный текст в надпись "результат" и покажем нужную панель '''
+    lb_Result.setText(res)
+    show_result()
+
+
+def check_answer():
+    ''' если выбран какой-то вариант ответа, то надо проверить и показать панель ответов'''
+    if answers[0].isChecked() or answers[1].isChecked() or answers[2].isChecked() or answers[3].isChecked():
+        # правильный ответ!
+        show_correct('Продолжай')
+
+
+
+def next_question():
+    ''' задает следующий вопрос из списка '''
+    # этой функции нужна переменная, в которой будет указываться номер текущего вопроса
+    # эту переменную можно сделать глобальной, либо же сделать свойством "глобального объекта" (app или window)
+    # мы заведем (ниже) свойство window.cur_question.
+    window.cur_question = window.cur_question + 1 # переходим к следующему вопросу
+    if window.cur_question >= len(questions_list):
+        window.cur_question = 0 # если список вопросов закончился - идем сначала
+    q = questions_list[window.cur_question] # взяли вопрос
+    ask(q) # спросили
+
+
+def click_OK():
+    ''' определяет, надо ли показывать другой вопрос либо проверить ответ на этот '''
+    if btn_OK.text() == 'Выбрать':
+        check_answer() # проверка ответа
+    else:
+        next_question() # следующий вопрос
+
 
 window = QWidget()
-window_2 = QWidget()
 window.setLayout(layout_card)
-window_2.setLayout(layout_card)
 window.setWindowTitle('Project "Evangelion"')
-window_2.setWindowTitle('Project "Evangelion"')
-window.cur_question = -1
-btn_OK.clicked.connect(click_OK)
-btn_OK.clicked.connect(click_OK_2)
-next_question()
-window.resize(600, 600)
-window_2.resize(600, 600)
+window.cur_question = -1    
 
+
+btn_OK.clicked.connect(click_OK) 
+
+
+# все настроено, осталось задать вопрос и показать окно:
+next_question()
+window.resize(400, 300)
 window.show()
 app.exec()
